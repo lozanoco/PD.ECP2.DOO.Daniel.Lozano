53c1a84c0afa4b4fe46c429ea73ecfac68051461
Daniel Lozano <lozanoco@gmail.com>
2014-10-23 17:41:14 +0200

    Implementados metodos abstractos acorde a los posibles acciones de los estados

--- src/main/java/state/connection/Estado.java
+++ src/main/java/state/connection/Estado.java
@@ -2,4 +2,10 @@ package state.connection;
 
 public abstract class Estado {
 
+	public abstract void abrir(Conexion conexion);
+	public abstract void cerrar(Conexion conexion);
+	public abstract void parar(Conexion conexion);
+	public abstract void iniciar(Conexion conexion);
+	public abstract void enviar(String msg,Conexion conexion);
+	public abstract void parar(int respuesta, Conexion conexion);
 }
