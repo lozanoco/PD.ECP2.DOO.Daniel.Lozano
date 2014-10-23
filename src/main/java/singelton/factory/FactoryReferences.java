b38439d264b0a9bdda7ee6beddd4aef94c64956f
Daniel Lozano <lozanoco@gmail.com>
2014-10-23 16:35:59 +0200

    Modificada la clase para que solo se pueda crear una fasctoria

--- src/main/java/singelton/factory/FactoryReferences.java
+++ src/main/java/singelton/factory/FactoryReferences.java
@@ -3,28 +3,37 @@ package singelton.factory;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class FactoryReferences {
-    private Map<String, Integer> references;
+	private static FactoryReferences factoryReferences=null;
+	private Map<String, Integer> references;
 
-    private int reference;
+	private int reference;
 
-    public FactoryReferences() {
-        this.references = new HashMap<>();
-        this.reference = 0;
-    }
+	private FactoryReferences() {
+		this.references = new HashMap<>();
+		this.reference = 0;
+	}
 
-    public int getReference(String key) {
-        Integer result = this.references.get(key);
-        if (result == null) {
-            this.references.put(key, this.reference);
-            result = this.reference;
-            reference++;
-        }
-        return result;
-    }
+	public static FactoryReferences getFactory(){
+		if(FactoryReferences.factoryReferences==null){
+			FactoryReferences.factoryReferences=new FactoryReferences();
+		}
+		return factoryReferences;    	
+	}
 
-    public void removeReference(String key) {
-        this.references.remove(key);
-    }
+	public int getReference(String key) {
+		Integer result = this.references.get(key);
+		if (result == null) {
+			this.references.put(key, this.reference);
+			result = this.reference;
+			reference++;
+		}
+		return result;
+	}
+
+	public void removeReference(String key) {
+		this.references.remove(key);
+	}
 
 }
