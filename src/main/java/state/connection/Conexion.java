f11a150f8118d58debfc5c6c24ad1e6a7d9efdc8
Daniel Lozano <lozanoco@gmail.com>
2014-10-23 17:49:10 +0200

    Añadido link a enviar

--- src/main/java/state/connection/Conexion.java
+++ src/main/java/state/connection/Conexion.java
@@ -36,34 +36,11 @@ public class Conexion {
     }
 
     public void enviar(String msg) {
-        if (this.estado == Estado.CERRADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PREPARADO) {
-            this.link.enviar(msg);
-            this.estado = Estado.ESPERANDO;
-        } else if (this.estado == Estado.ESPERANDO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
+        estado.enviar(msg, this);    
         }
-        assert false : "estado imposible";
-    }
 
     public void recibir(int respuesta) {
-        if (this.estado == Estado.CERRADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PREPARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.ESPERANDO) {
-            if (respuesta == 0) {
-                this.estado = Estado.PREPARADO;
-            } else {
-                this.estado = Estado.CERRADO;
-            }
-        }
-        assert false : "estado imposible";
+       estado.recibir(respuesta,link, this);
     }
 
 }
--- src/main/java/state/connection/Estado.java
+++ src/main/java/state/connection/Estado.java
@@ -7,5 +7,5 @@ public abstract class Estado {
 	public abstract void parar(Conexion conexion);
 	public abstract void iniciar(Conexion conexion);
 	public abstract void enviar(String msg,Conexion conexion);
-	public abstract void parar(int respuesta, Conexion conexion);
+	public abstract void recibir(int respuesta,Link link, Conexion conexion);
 }
